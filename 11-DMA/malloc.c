3f7b28c7531e4eecf84a29f76d6ed00ae02cdd4e
atkaridarshan04 <atkaridarshan04@gmail.com>
2024-12-07 21:26:10 +0530

    update DMA

--- 11-DMA/malloc.c
+++ 11-DMA/malloc.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 int main()
 {
     int *ptr;
     int num;
 
-    printf("Enter the number of storage you want: ");
+    printf("Enter the number of integers you want storage for: ");
     scanf("%d", &num);
 
     ptr = (int *)malloc(num * sizeof(int));
 
     for (int i = 0; i < num; i++)
     {
+        // printf("%d\n", ptr[i]);
         printf("Enter the value of %d element:", i + 1);
         scanf("%d", &ptr[i]);
+        
     }
 
     printf("Elements are\n");
